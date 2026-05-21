62a03e01447a27f119c8fa3705cda6c552c6a35b
Mukilanbalakrishnan <154340556+Mukilanbalakrishnan@users.noreply.github.com>
2026-05-21 11:56:47 +0530

    mobile-biew

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
 
   return (
     <footer className="bg-navy-950 pt-20 pb-6 relative border-t border-white/5">
-      <div className="max-w-7xl mx-auto px-6">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6">
         
         {/* Main Content Section */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12 mb-16 relative">
